e1513abba897fb90cb71c69e9317efc6884627e1
Axel Polleux <apolleux@2E7.42angouleme.fr>
2025-12-09 15:07:15 +0100

    GNL working

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: apolleux <apolleux@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/27 15:39:14 by apolleux          #+#    #+#             */
-/*   Updated: 2025/12/08 18:21:37 by apolleux         ###   ########.fr       */
+/*   Updated: 2025/12/09 14:38:29 by apolleux         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,11 +31,8 @@ static char	*read_line(int fd, char *stash)
 			return (0);
 		}
 		else if (read_return == 0)
-		{
-			free(buffer);
-			stash = NULL;
-			return (stash);
-		}
+			break ;
+		buffer[read_return] = '\0';
 		stash = ft_strjoin(stash, buffer);
 	}
 	free(buffer);
@@ -46,9 +43,13 @@ static char	*border_line(char *stash)
 {
 	char	*res;
 
-	res = stash;
+	res = NULL;
+	if (!stash)
+		return (0);
 	if (ft_strichr(stash, '\n') >= 0)
 		res = ft_substr(stash, 0, ft_strichr(stash, '\n') + 1);
+	else
+		res = ft_substr(stash, 0, ft_strlen(stash));
 	return (res);
 }
 
@@ -57,34 +58,31 @@ static char	*dead_line(char *stash)
 	char	*res;
 
 	res = NULL;
-	if (ft_strichr(stash, '\0') >= 0)
-	{
-		free(stash);
+	if (!stash)
 		return (0);
-	}
-	else if (ft_strichr(stash, '\n') >= 0)
-		res = ft_substr(stash, ft_strichr(stash, '\n') + 1, ft_strlen(stash));
+	if (ft_strichr(stash, '\n') >= 0)
+		res = ft_substr(stash, ft_strichr(stash, '\n') + 1,
+				(ft_strlen(stash) - ft_strichr(stash, '\n') + 1));
 	free(stash);
 	return (res);
 }
 
 char	*get_next_line(int fd)
 {
-	static char	*stash;
+	static char	*stash = NULL;
 	char		*line;
 
 	line = NULL;
-	if (fd < 0 || fd > 1024 || BUFFER_SIZE <= 0)
+	if (fd < 0 || fd >= 1024 || BUFFER_SIZE <= 0)
 		return (0);
-	if (!stash)
-	{
-		stash = ft_calloc(BUFFER_SIZE + 1, sizeof(char));
-		stash = read_line(fd, stash);
-	}
-
-	else if (ft_strichr(stash, '\n') == -1)
+	if (ft_strichr(stash, '\n') == -1)
 		stash = read_line(fd, stash);
 	line = border_line(stash);
 	stash = dead_line(stash);
+	if (stash && stash[0] == '\0')
+	{
+		free(stash);
+		stash = NULL;
+	}
 	return (line);
 }
